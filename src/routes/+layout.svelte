5685de05404a53a5df73111b79c217453e34d7ed
ArthurGerman <arthurgermanotutu@hotmail.com>
2025-07-14 18:23:02 -0300

    add novas músicas

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,18 +7,29 @@
     let audio: HTMLAudioElement;
     $: audioRef.set(audio);
 
+    // 3 músicas que vão tocar em loop
+    let musicas : string[] = ["/music/music_2.mp3","/music/music_1.mp3","/music/music_3.mp3"] // Array com os endereços das músicas
+    let musica_atual = 0
+
+    function trocar_musica() {
+        musica_atual = (musica_atual + 1) % musicas.length;
+        audio.src = musicas[musica_atual];
+        audio.play().catch(() => {});
+    }
 
     // Inicia a música quando o componente é montado
 	onMount(() => {
         if (audio) {
+            audio.src = musicas[musica_atual]
             audio.muted = false; // inicia mutado para autoplay funcionar
             audio.volume = 1;
-            audio.play().catch(() => {});
+            audio.play().catch(() => {})
+            audio.onended = trocar_musica;
         }
     });
 </script>
 
-<audio bind:this={audio} src="/musica.mp3" preload="auto" loop></audio>
+<audio bind:this={audio} preload="auto"></audio>
 
 
 <div class:dark-theme={$tema === 'dark'}>
