7a8d1f66c0b5696f98ad51e2e4be160fd877d096
ArthurGerman <arthurgermanotutu@hotmail.com>
2025-07-15 16:15:08 -0300

    add novas palavras

--- src/palavras.ts
+++ src/palavras.ts
@@ -1,16 +1,12 @@
-export const palavras = [
-    "CASAS", "LIVRO", "TORRE", "FALTA", "BOMBA", 
-    "TERRA", "TERNO", "ONTEM", "MORTE", "CARRO", 
-    "ANDAR", "ALTAR", "GATOS", "CALMA", "ARMAS", 
-    "PONTO", "LINHA", "PERTO", "LONGE", "PRETO",
-    "VAPOR", "FOCOS", "TRAPO", "LIMPO", "BEIJO", 
-    "FUNDO", "RITMO", "VIDAS", "SUSTO", "GRAMA", 
-    "CALOR", "LUGAR", "SINAL", "PORTA", "NUVEM", 
-    "ROSTO", "VERBO", "REINO", "SORTE", "AMIGO", 
-    "FOLHA", "TEMPO", "MUNDO", "VINHO", "NOITE", 
-    "BANHO", "CORDA", "GRUTA", "LUCRO", "TARDE", 
-    "CAMPO", "PERNA", "PRAIA", "GENTE", "JUSTO", 
-    "SONHO", "RUBRO", "CLARO", "BAIXO", "CHEIO", 
-    "TRAMA", "FARDO", "REVER", "BANCO", "TRIGO", 
-    "FORMA", "VERDE", "DENTE", "TENSO", "RAIVA",
+export const palavras = [ // Array com atualmente 100 palavras
+    "CASAS", "LIVRO", "TORRE", "FALTA", "BOMBA", "TERRA", "TERNO", "ONTEM", "MORTE", "CARRO", 
+    "ANDAR", "ALTAR", "GATOS", "CALMA", "ARMAS", "PONTO", "LINHA", "PERTO", "LONGE", "PRETO",
+    "VAPOR", "FOCOS", "TRAPO", "LIMPO", "BEIJO", "FUNDO", "RITMO", "VIDAS", "SUSTO", "GRAMA", 
+    "CALOR", "LUGAR", "SINAL", "PORTA", "NUVEM", "ROSTO", "VERBO", "REINO", "SORTE", "AMIGO", 
+    "FOLHA", "TEMPO", "MUNDO", "VINHO", "NOITE", "BANHO", "CORDA", "GRUTA", "LUCRO", "TARDE", 
+    "CAMPO", "PERNA", "PRAIA", "GENTE", "JUSTO", "SONHO", "RUBRO", "CLARO", "BAIXO", "CHEIO", 
+    "TRAMA", "FARDO", "REVER", "BANCO", "TRIGO", "FORMA", "VERDE", "DENTE", "TENSO", "RAIVA",
+    "CASAL", "TECLA", "FELIZ", "GRATO", "TOMBO", "VIRAR", "CINZA", "PONTE", "RISCO", "FALHA", 
+    "CERTO", "FREIO", "LENTO", "PLANO", "RURAL", "AREIA", "TINHA", "NORTE", "VAGAS", "METRO", 
+    "DADOS", "CARRO", "LOUCA", "RAIOS", "POEMA", "JOGOS", "POMBO", "MARTE", "MACHO", "POLVO"
 ];
