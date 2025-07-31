52b084fde1c85e0d3c93815b2cabb86e8df76829
ArthurGerman <arthurgermanotutu@hotmail.com>
2025-07-31 19:05:32 -0300

    modificação API da versão atual do jogo

--- src/routes/api/commits/+server.ts
+++ src/routes/api/commits/+server.ts
@@ -1,7 +1,14 @@
 // Função que busca a quantidade de commits
 
 export async function GET() {
-    const res = await fetch('https://api.github.com/repos/ArthurGerman/projeto_letrinha/commits');
-    const commits = await res.json();
-    return new Response(JSON.stringify({ total: Array.isArray(commits) ? commits.length : 0 }));
+    const res = await fetch('https://api.github.com/repos/ArthurGerman/projeto_letrinha/commits?per_page=1');
+    const link = res.headers.get('link');
+    let total = 1;
+    if (link) {
+        const match = link.match(/&page=(\d+)>; rel="last"/);
+        if (match) {
+            total = parseInt(match[1]);
+        }
+    }
+    return new Response(JSON.stringify({ total }));
 }
